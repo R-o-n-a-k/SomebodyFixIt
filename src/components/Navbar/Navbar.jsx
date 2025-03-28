f20cad8c2f0145742d427d0e2794a558c943445a
Ronak <rroonnaakk7@gmail.com>
2025-03-29 00:44:57 +0530

    ref: renaming logo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import logo from "../../assets/images/somebodyfixit.svg";
+import logo from "../../assets/images/navbarLogo.svg";
 import { Link, useLocation } from "react-router-dom";
 
 function Navbar({ token }) {
